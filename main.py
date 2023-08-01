0f151617abf91a6bbdc704a2c8d15c0e5d788073
mirisinger <hand1b14@mbjcomp.org.il>
2023-08-01 20:20:51 +0300

    finish in create network

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, FastAPI
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, UploadFile, File
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from Authentication.login import is_technician
 from Controllers.network_controller import make_network, get_network
@@ -16,14 +16,22 @@ NETWORK_PATH = "/network"
 USER_PATH = "/technician"
 
 
+@app.get("/")
+async def root():
+    return "welcome to team 6 project!"
+
+
 @app.post(f"{USER_PATH}/login")
 async def login(password, user_name):
     return await is_technician(password, user_name)
 
 
 @app.post(f"{NETWORK_PATH}/create_network")
-async def create_network(pcap_file, client_id, date_taken, premise_name, technician_name):
-    return await make_network(pcap_file, client_id, date_taken, premise_name, technician_name)
+async def create_network(file: UploadFile = File(...), client_id: int = Form(...), premise: str = Form(...), technician_name: str = Form(...)):
+    file_content = await file.read()
+    date_taken = '2023-07-01'
+    is_success = await make_network(file_content, client_id, premise, date_taken, technician_name)
+    return is_success
 
 
 @app.get(f"{NETWORK_PATH}/view_network")
@@ -36,7 +44,7 @@ async def get_filtered_devices(network_id, condition):
     return await view_filter_devices(network_id, condition)
 
 
-#NTH
+# NTH
 @app.get(f"{NETWORK_PATH}/get_client_devices")
 async def get_client_devices(client_id):
     return await view_client_devices(client_id)
